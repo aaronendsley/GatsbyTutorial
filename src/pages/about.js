31e88723f46f2c5a9046ef662e66acda6f657a0e
Aaron Endsley <aaronendsley@gmail.com>
2020-01-25 14:31:14 -0600

    learning about building pages

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "../components/layout";
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Computer, belay that order. We finished our first sensor sweep of the
+        neutral zone. For an android with no feelings, he sure managed to evoke
+        them in others. We could cause a diplomatic crisis. Take the ship into
+        the Neutral Zone Yes, absolutely, I do indeed concur, wholeheartedly!
+        Could someone survive inside a transporter buffer for 75 years? When has
+        justice ever been as simple as a rule book? Did you come here for
+        something in particular or just general Riker-bashing? Sorry, Data. We
+        know you're dealing in stolen ore. But I wanna talk about the
+        assassination attempt on Lieutenant Worf.
+      </p>
+    </div>
+  </Layout>
+);
+
+export default About;
